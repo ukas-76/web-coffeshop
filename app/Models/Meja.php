4bfb9904f2534e3a19d5e0a480f547bc8aa96f08
Mr.2wir <saptaaditya76@gmail.com>
2026-06-21 21:35:00 +0700

    Melengkapi dashboard admin & login admin

--- app/Models/Meja.php
+++ app/Models/Meja.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Meja extends Model
 {
     protected $table = 'meja';
-    protected $fillable = ['nomor_meja', 'kapasitas', 'status'];
+    protected $fillable = [
+        'nomor_meja',
+        'kapasitas',
+        'status',
+        'gambar_lokasi', // Tambahkan ini
+    ];
 }
